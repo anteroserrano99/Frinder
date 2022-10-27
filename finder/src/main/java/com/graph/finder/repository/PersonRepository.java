c2e00e11605d02f93374e3237c86d0899fe9386b
anteroserrano99 <serranoantero13@gmail.com>
2022-10-27 12:09:42 +0200

    Added PathRangeRepository to support Path range searches

--- finder/src/main/java/com/graph/finder/repository/PersonRepository.java
+++ finder/src/main/java/com/graph/finder/repository/PersonRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.neo4j.repository.query.Query;
 
-public interface PersonRepository extends Neo4jRepository<Person, String> {
+public interface PersonRepository extends Neo4jRepository<Person, String>, PathRangeRepository {
 
   @Query("Match (n:Person) RETURN n")
   List<Person> getAllPerson();
